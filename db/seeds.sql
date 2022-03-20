ac09088430af383049abc42da5fd207b056d68d8
Girish Madnani <girishmadnani05@gmail.com>
2022-03-20 18:45:50 +0000

    seeds sql adding employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,13 @@ VALUES  (1, "Sales Lead", 100000.00, 1),
         (6, "Accountant", 125000.00, 3),
         (7, "Legal Team Lead", 250000.00, 4),
         (8, "Lawyer", 190000.00, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (1, "John", "Doe", 1, NULL),
+        (2, "Mike", "Chan", 2, 1),
+        (3, "Ashley", "Rodriguez", 3, NULL),
+        (4, "Kevin", "Tupik", 4, 3),
+        (5, "Kunal", "Singh", 5, NULL),
+        (6, "Malia", "Brown", 6, 5),
+        (7, "Sarah", "Lourd", 7, NULL),
+        (8, "Tom", "Allen", 8, 7);
