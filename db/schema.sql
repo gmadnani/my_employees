dd453233b5f958fee9b791998ad580e8e4c5c947
Girish Madnani <girishmadnani05@gmail.com>
2022-03-20 18:09:17 +0000

    role table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,21 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-    id INT not null,
-    name VARCHAR(30) not null
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
+
+CREATE TABLE role(
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+
+
