38aff8e5705e3654cc85d5f4df1f3123f174e939
Girish Madnani <girishmadnani05@gmail.com>
2022-03-20 18:12:20 +0000

    employee table

--- db/schema.sql
+++ db/schema.sql
@@ -20,5 +20,17 @@ CREATE TABLE role(
     ON DELETE SET NULL
 );
 
-
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
 
