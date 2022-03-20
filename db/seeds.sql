0a6904fa133c7ec7a9503206bc39e00bc102dcfb
Girish Madnani <girishmadnani05@gmail.com>
2022-03-20 18:41:46 +0000

    seeds sql adding roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,13 @@ VALUES  (1, "Sales"),
         (2, "Engineering"),
         (3, "Finance"),
         (4, "Legal");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES  (1, "Sales Lead", 100000.00, 1),
+        (2, "Salesperson", 80000.00, 1),
+        (3, "Lead Engineer", 150000.00, 2),
+        (4, "Software Engineer", 120000.00, 2),
+        (5, "Account Manager", 160000.00, 3),
+        (6, "Accountant", 125000.00, 3),
+        (7, "Legal Team Lead", 250000.00, 4),
+        (8, "Lawyer", 190000.00, 4);
