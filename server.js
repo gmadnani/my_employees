6142b558c1a942ea22e7eae9c20a3b7c179e43ee
Girish Madnani <girishmadnani05@gmail.com>
2022-03-20 19:33:00 +0000

    inquirer

--- server.js
+++ server.js
@@ -9,6 +9,54 @@ const db = mysql.createConnection(
         port: 3306,
         password: '1234567890',
         database: 'employee_db'
-    },
-    console.log(`Connected to the employee_db database.`)
-);
+    });
+
+db.connect(err => {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Exit"
+        ]
+    }).then((answer) => {
+        switch (answer.action) {
+            case "View all employees":
+                break;
+
+            case "Add an employee":
+                break;
+
+            case "Update employee role":
+                break;
+
+            case "View all roles":
+                break;
+
+            case "Add a role":
+                break;
+
+            case "View all departments":
+                break;
+
+            case "Add a department":
+                break;
+
+            case "Exit":
+                db.end();
+                break;
+        }
+    });
+}
